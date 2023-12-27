e111b11ab78917ac264ecebcc0c77b0669c63be2
Leijten-git <floris.leijten@fao.org>
2023-12-27 09:58:45 +0100

    added master script

---
+++ master_script.R
@@ -0,0 +1,41 @@
+library(devtools)
+library(FLUTES)
+
+# user input --------------------------------------------------------------
+
+base_directory <- "" # path to software package
+package_name <- "FLUTES" # specify packahe name
+dir_lu_filenames <- "" # path to filenames (raster files) with land use types
+
+path_cloud <- "" # path to cloud folder
+base_dir <- paste0(path_cloud, "/") # path output dir
+path_lu_legend_filename <- file.path("") # path csv file with land use keys and labels
+
+# leave as is -------------------------------------------------------------
+
+file_path_pkg <- paste0(base_directory, "/", package_name)
+load_all(path = file_path_pkg)
+
+setwd(dir_lu_filenames)
+file_names <- list.files()[grepl("LCCS", list.files())]
+paths_lu_filenames <- paste0(getwd(), "/", file_names)
+years <- as.numeric(stringr::str_sub(sub('.*P1Y', '', paths_lu_filenames), 2, 5))
+
+
+# run FLUTES --------------------------------------------------------------
+
+extract_country_names() # list all country names
+
+country = "" # specify country of interest
+
+run_FLUTES(.country = country,
+           .years = years,
+           .paths_lu_filenames = paths_lu_filenames,
+           .dir_output_files = base_dir,
+           .aggregation_factor = 32,
+           .path_lu_legend_filename = path_lu_legend_filename,
+           .cut_off_year = 1998, # 1999 - 2020
+           .lags = c(1),
+           .growth_constraint = NULL, # NULL; numeric value; or "cal" (calculate)
+           .vals_lu_classes_to_exclude = 210) # water bodies
+
