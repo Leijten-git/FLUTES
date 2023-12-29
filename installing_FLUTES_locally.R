4056fa3c60cc36faad24e219e247d103e6497a8c
Leijten-git <floris.leijten@fao.org>
2023-12-29 10:01:53 +0100

    add script for installing FLUTES locally

---
+++ installing_FLUTES_locally.R
@@ -0,0 +1,4 @@
+install.packages("", # specify file path to package folder   
+                 repos = NULL, # Can be NULL to install from local files
+                 dependencies = T,
+                 type = "source")
